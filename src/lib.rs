e780017970af1bd27717dc09f330f310476b6830
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2026-09-04 09:15:31 -0400

    Warn on suspect matches

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ mod search_items;
 use memchr::memmem;
 use std::time::Instant;
 
-static SEARCH_BOUND: usize = 0x1000;
+static SEARCH_BOUND: usize = 0x1000; // Can be increased if we don't find it
+static MAX_HAMMING_DISTANCE: u64 = 4; // Can be increased if we don't find it
 
 #[derive(Clone)]
 struct HookAddress {
@@ -32,11 +33,20 @@ impl HookAddress {
                 "[OffsetLocator] Found exact match for {} at offset {:#x}",
                 self.name, offset
             );
-        } else if let Some((offset, bytes)) = fuzzy_search(self.old_offset, self.bytes) {
-            println!(
-                "[OffsetLocator] Found fuzzy match for {} at offset {:#x}",
-                self.name, offset
-            );
+        } else if let Some((offset, bytes, hamming_dist)) =
+            fuzzy_search(self.old_offset, self.bytes)
+        {
+            if hamming_dist <= MAX_HAMMING_DISTANCE {
+                println!(
+                    "[OffsetLocator] Found fuzzy match for {} at offset {:#x}",
+                    self.name, offset
+                );
+            } else {
+                println!(
+                    "[OffsetLocator] Found suspicious fuzzy match for {} at offset {:#x} with hamming distance {}. This probably is incorrect.",
+                    self.name, offset, hamming_dist
+                )
+            }
             println!(
                 "[OffsetLocator] Update {} search bytes to {:#x?}",
                 self.name, bytes
@@ -67,7 +77,7 @@ pub fn exact_search(needle: &[u8]) -> Option<usize> {
     if first == last { Some(first) } else { None }
 }
 
-pub fn fuzzy_search(old_offset: Option<usize>, needle: &[u8]) -> Option<(usize, &[u8])> {
+pub fn fuzzy_search(old_offset: Option<usize>, needle: &[u8]) -> Option<(usize, &[u8], u64)> {
     let search_window_start = old_offset.unwrap_or(0).saturating_sub(SEARCH_BOUND);
     let haystack = unsafe {
         let region_start = skyline::hooks::getRegionAddress(skyline::hooks::Region::Text) as usize;
@@ -89,10 +99,11 @@ pub fn fuzzy_search(old_offset: Option<usize>, needle: &[u8]) -> Option<(usize, 
     let ret = haystack
         .windows(needle.len())
         .enumerate()
-        .min_by_key(|&(_, w)| (hamming::distance_fast(w, needle).unwrap_or(u64::MAX), w));
+        .map(|(ind, bytes)| (ind, bytes, hamming::distance(bytes, needle)))
+        .min_by_key(|&(_, _, hamming_dist)| hamming_dist);
 
-    if let Some((ind, bytes)) = ret {
-        Some((ind + search_window_start, bytes))
+    if let Some((ind, bytes, hamming_dist)) = ret {
+        Some((ind + search_window_start, bytes, hamming_dist))
     } else {
         println!(
             "[OffsetLocator] Haystack is empty! Check that the offset {:#x?} is valid?",
